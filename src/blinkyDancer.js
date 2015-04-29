20043225a29c21374afe13d0696b1dc027885f97
MKS-Student <mks.student@hackreactor.com>
2015-04-28 17:58:11 -0700

    Enabled lineup button and gliding dancers

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,4 +1,3 @@
-
 var BlinkyDancer = function(top, left, timeBetweenSteps){
   Dancer.call(this, top, left, timeBetweenSteps); // ??? for args
   this.top = top;
@@ -12,54 +11,3 @@ BlinkyDancer.prototype.step = function() {
   Dancer.prototype.step.call(this);
   this.$node.toggle();
 }// Dancer.step + toggle
-
-
-/*
-var makeBlinkyDancer = function(top, left, timeBetweenSteps){
-  var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
-
-  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
-  // so we must keep a copy of the old version of this function
-
-  var oldStep = blinkyDancer.step;
-
-    blinkyDancer.step = function(){
-      // call the old version of step at the beginning of any call to this new version of step
-      oldStep();
-      // toggle() is a jQuery method to show/hide the <span> tag.
-      // See http://api.jquery.com/category/effects/ for this and
-      // other effects you can use on a jQuery-wrapped html tag.
-      blinkyDancer.$node.toggle();
-    };
-
-    return blinkyDancer;
-  };
-*/
-
-
-////////////////////////////////////////////////////////////////////////////////////////
-
-// var Dancer = function(top, left, timeBetweenSteps) {
-//   this.top = top;
-//   this.left = left;
-//   this.timeBetweenSteps = timeBetweenSteps;
-//   this.$node = $('<span class="dancer"></span>');
-//   this.step();
-//   this.setPosition(top, left);
-
-// }
-
-// Dancer.prototype.step = function(){
-//   setTimeout(step, this.timeBetweenSteps);
-// };
-
-// Dancer.prototype.setPosition = function(top, left){
-//  var styleSettings = {
-//       top: top,
-//       left: left
-//     };
-//     this.$node.css(styleSettings);
-//   };
-// }
-
-
--- src/init.js
+++ src/init.js
@@ -1,3 +1,4 @@
+window.clicked = false;
 $(document).ready(function(){
   window.dancers = [];
 
@@ -17,69 +18,24 @@ $(document).ready(function(){
      */
     // console.log($this.data("dancer-maker-function-name"));
     var dancerMakerFunctionName = $(this).data("dancer-maker-function-name");
-    console.log(dancerMakerFunctionName)
 
     // get the maker function for the kind of dancer we're supposed to make
     var dancerMakerFunction = window[dancerMakerFunctionName];
 
     // make a dancer with a random position
-    if (dancerMakerFunctionName === "AnimatedDancer"){
-      var dancer = new dancerMakerFunction(
-        $("body").height() * Math.random(),
-        $("body").width() * Math.random(),
-        1000 * Math.random()
+    var dancer = new dancerMakerFunction(
+      $("body").height() * Math.random(),
+      $("body").width() * Math.random(),
+      1000
       );
-      console.log("style:", dancer.$node[0].style.left)
-      $('body').append(dancer.$node);
-      
-      console.log("dancer"); 
-    }
-    else{
-      var dancer = new dancerMakerFunction(
-        $("body").height() * Math.random(),
-        $("body").width() * Math.random(),
-        Math.random() * 1000
-        );
-      $('body').append(dancer.$node);
-    }
 
+    $('body').append(dancer.$node);
+    window.dancers.push(dancer);
+    console.log(window.dancers);
   });
-
-
-
-
-
-
-
-
-// $(".addOurDancerButton").on("click", function(event){
-//     /* This function sets up the click handlers for the create-dancer
-//      * buttons on dancefloor.html. You should only need to make one small change to it.
-//      * As long as the "data-dancer-maker-function-name" attribute of a
-//      * class="addDancerButton" DOM node matches one of the names of the
-//      * maker functions available in the global scope, clicking that node
-//      * will call the function to make the dancer.
-//      */
-
-//      dancerMakerFunctionName is a string which must match
-//      * one of the dancer maker functions available in global scope.
-//      * A new object of the given type will be created and added
-//      * to the stage.
-     
-//      $(this).show("slide", {direction:"left"}, 1000);
-//     var dancerMakerFunctionName = $(this).data("dancer-maker-function-name");
-
-//     // get the maker function for the kind of dancer we're supposed to make
-//     var dancerMakerFunction = window[dancerMakerFunctionName];
-
-//     // make a dancer with a random position
-
-//     var dancer = new dancerMakerFunction(
-//       $("body").height() * Math.random(),
-//       $("body").width() * Math.random(),
-//       Math.random() * 1000
-//     );
-//     $('body').append(dancer.$node);
-//   });
+  
+  $(".lineUp").on("click", function(event) {
+    clicked = true;
+  })
 });
 
